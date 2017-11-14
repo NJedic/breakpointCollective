36404132056cf41b2324ce1e94195535cc690202
Nikki Jedic <nikki.jedic@gmail.com>
2017-11-13 21:22:46 -0500

    about me section completed

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,14 +1,32 @@
+// INDEX.HTML
+// =================================================================
+// =================================================================
+
 // VARIABLES
 // +++++++++++++++++++++++++++
 var enterHr = document.getElementById("enterHr");
-
 
 // FUNCTIONS
 // +++++++++++++++++++++++++++
 
 // On document load and ready...
 $(document).ready(function(){
+	// ...shift hr to the right
 	TweenLite.to(enterHr, 2.5, {width:500});
-})
+});
 
-	
+
+// HOME.HTML
+// =================================================================
+// =================================================================
+
+// FUNCTIONS
+// +++++++++++++++++++++++++++
+
+// On document load and ready...
+$(document).ready(function(){
+	// ...add class to fadeIn hr
+	$("#homeHr").addClass("animated fadeIn delay2s9ms");
+});
+
+
